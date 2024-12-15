eafb1614964244efbd93100f15fa773a019d8acb
driversline <n526258360@gmail.com>
2024-12-15 13:39:48 +0300

    mods-1.21.1.sh

--- 1.21.1/mods-1.21.1.sh
+++ 1.21.1/mods-1.21.1.sh
@@ -2,6 +2,6 @@
 
 sudo pacman -S jdk-openjdk
 
-cd ~/Downloads/minecraft && java -jar fabric-installer.jar client -v 1.21.1 -mcversion 1.21.1
+cd ~/minecraft && java -jar fabric-installer.jar client -v 1.21.1 -mcversion 1.21.1
 
 exit 0
